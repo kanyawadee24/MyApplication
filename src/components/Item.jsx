bbed7a8a9ff4a2c3de859e0c2165cf9c6a1c72bd
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-06-19 01:03:57 +0700

    finish day3

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,18 @@
+import PropTypes from "prop-types";
+
 const Item = (props) => {
-  const {title,amount} = props
+  const { title, amount } = props;
   return (
     <li>
       {title}
       <span>{amount}</span>
     </li>
   );
+};
+
+Item.propTypes = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
 };
 
 export default Item;
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -4,17 +4,19 @@ import { v4 as uuidv4 } from "uuid";
 
 const Transaction = () => {
   const data = [
-    { title: "ค่ารักษาพยาบาล", amount: "2000" },
-    { title: "ค่าน้ำมัน", amount: "5000" },
-    { title: "ค่าเช่าบ้าน", amount: "80000" },
-    { title: "เงินเดือน", amount: "890" },
-    { title: "ค่าประกัน", amount: "300" },
-    { title: "ค่าเดินทาง", amount: "1500" },
+    { title: "ค่ารักษาพยาบาล", amount: 2000 },
+    { title: "ค่าน้ำมัน", amount: 5000 },
+    { title: "ค่าเช่าบ้าน", amount: 80000 },
+    { title: "เงินเดือน", amount: 890 },
+    { title: "ค่าประกัน", amount: 300 },
+    { title: "ค่าเดินทาง", amount: 1500 },
   ];
   return (
     <ul className="item-list">
       {data.map((element) => {
-        return <Item {...element} key={uuidv4} />;
+        return (
+          <Item title={element.title} amount={element.amount} key={uuidv4()} />
+        );
       })}
     </ul>
   );
